119e090e34dac94f3583ce205d53bd9133647067
bkosciow <kosci1@gmail.com>
2025-06-21 12:22:27 +0200

    relay example

---
+++ example/relay_main_example.py
@@ -0,0 +1,40 @@
+from config import *
+import microplate.core as core
+
+from microplate.relay_worker import RelayWorker
+from microplate.relay_hanlder import RelayHandler
+from microplate.button_worker import ButtonWorker
+# from microplate.debug_handler import DebugHandler
+from microplate.network_worker import NetworkWorker
+
+
+print("booting")
+
+
+relay_worker = RelayWorker(RELAY)
+r = RelayHandler(relay_worker)
+
+# callback for buttons to toggle relays
+def click_callback(pin):
+    print("pin : ", pin)
+    rr = r.workers[0].relays[0]
+    print(rr)
+    r.workers[0].toggle(0, not rr['current'])
+
+# worker for buttons
+btns = ButtonWorker()
+btns.add_button(18, 400, click_callback)
+
+# network keep alive
+net = NetworkWorker()
+
+# add workers to core
+core.add_worker(relay_worker)
+core.add_worker(btns)
+core.add_worker(net)
+
+# ad handler to core
+core.add_handler("relay", r)
+
+# start core
+core.start()
