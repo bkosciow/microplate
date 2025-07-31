55ab844040dd065329ba13ee44db183887f89d92
bkosciow <kosci1@gmail.com>
2025-07-31 10:36:21 +0200

    hd 44780 IoTv1 handler

--- microplate/hd44780_handler.py
+++ microplate/hd44780_handler.py
@@ -6,8 +6,13 @@ class HD44780Handler(Handler, HomeAssistantHandler):
         super().__init__(dev)
 
     def handle(self, message):
-        if message["event"] == "lcd.set_content":
-            print(message)
+        if message["event"] == "lcd.set_text":
+            if 'clear' in message['parameters'] and message['parameters']['clear']:
+                self.workers[0].io.buffer_clear()
+            if 'xy' in message['parameters']:
+                self.workers[0].io.set_xy(message['parameters']['xy'][0], message['parameters']['xy'][1])
+            if 'text' in message['parameters']:
+                self.workers[0].io.stream(message['parameters']['text'])
 
     def handle_mqtt(self, topic, msg):
         self.workers[0].io.buffer_clear()
